33ccb2b6f17f46327b6adb1d118a50d1e4cf7559
scolarofterra <travisdeschenes@gmail.com>
2018-01-30 19:41:06 -0500

    week 2 part one add

---
+++ labs/Week2/lab2.py
@@ -0,0 +1,12 @@
+def squared_nums(num_list):
+	"""
+	Squares numbers in num_list
+	num_list: list of numbers
+	Returns: list of these numbers squared
+	"""
+	new_list[]
+	for num in num_list:
+		new_list.append(num^2)
+
+	return new_list
+
